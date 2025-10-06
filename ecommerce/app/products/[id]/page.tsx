2ea349c18d02308bf75a7d21d9e39795eac64e27
Muayad Fandqle <90987200+muayadcode@users.noreply.github.com>
2025-10-06 04:50:58 +0000

    adding tailwind styles

--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -7,11 +7,26 @@ export default function ProdcutDetailPage({ params }:{params:{id: string}}){
     return <NotFoundPage/>
     }
     return(
-      <>
-      <h1>{product.name}</h1>
-      <h3>${product.price}</h3>
-      <p>{product.description}</p>
-      </>
+      
+      <div className="container mx-auto p-8 flex flex-col md:flex-row">
+        <div className="md:w-1/2 mb-4 md:mb-0 md:mr-8">
+          <img 
+            src={'/' + product.imageUrl} 
+            alt=" Product Image" 
+            className=" w-full h-auto rounded-lg shadow-md"
+            />
+        </div>
+
+        <div className="md:w-1/2">
+          
+          <h1 className="text-4xl font-bold mb-4 ">{product.name}</h1>
+          <p className="text-2xl text-gray-600 mb-6">${product.price}</p>
+          <h3 className="text-2xl font-semibold mb-2 ">Description</h3>
+          <p className="text-gray-700"> {product.description}</p>
+        </div> 
+      </div>
+
+      
 
     ) 
 }
