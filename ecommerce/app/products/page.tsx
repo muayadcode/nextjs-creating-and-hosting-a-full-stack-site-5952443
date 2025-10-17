a4155341ef3705c996e294c3444c2c620dfd780b
Muayad Fandqle <90987200+muayadcode@users.noreply.github.com>
2025-10-17 04:24:18 +0000

    loading all products from database to the front end page

--- ecommerce/app/products/page.tsx
+++ ecommerce/app/products/page.tsx
@@ -1,7 +1,9 @@
 import ProductsList from "../ProductsList";
-import { products } from '../product-data';
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  const response = await fetch('https://legendary-sniffle-5p99qxwj4rrh446j-3000.app.github.dev//api/products');
+  const products = await response.json();
+
   return (
     <div className="container mx-auto p-8"> 
       <h1 className="text-4xl font-bold mb-8">Products</h1> 
