bc3272302a60b3bd0107c5792713e11159499234
Muayad Fandqle <90987200+muayadcode@users.noreply.github.com>
2025-10-20 03:44:07 +0000

    loading shopping cart items from data base

--- ecommerce/app/cart/page.tsx
+++ ecommerce/app/cart/page.tsx
@@ -1,28 +1,14 @@
 'use client';
 
-import { useState } from 'react';
-import { products } from '../product-data';
-import Link from 'next/link';
+import ShoppingCartList from "./ShoppingCartList";
 
-export default function CartPage() {
-  const [cartIds] = useState(['123', '345']); 
 
-  const cartProducts = cartIds.map(id => products.find(p => p.id === id)!);
 
-  return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-4xl font-bold mb-8">Shopping Cart</h1>
+export default async function CartPage() {
+ const response = await fetch ('https://legendary-sniffle-5p99qxwj4rrh446j-3000.app.github.dev/api/users/1/cart')
+ const cartProducts = await response.json();
 
-      <ul className="space-y-4"> {/* List for cart items */}
-        {cartProducts.map(product => (
-          <li key={product.id} className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300">
-            <Link href={`/products/${product.id}`}>
-              <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
-              <p className="text-gray-600">${product.price}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+
+ return <ShoppingCartList initialCartProducts={cartProducts} />
+
 }
