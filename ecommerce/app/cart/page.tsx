a31073303dd2f2b39a4ac70351cf27c6f6d7e921
Muayad Fandqle <90987200+muayadcode@users.noreply.github.com>
2025-10-20 05:27:38 +0000

    adding items to the cart

--- ecommerce/app/cart/page.tsx
+++ ecommerce/app/cart/page.tsx
@@ -1,14 +1,12 @@
-'use client';
-
 import ShoppingCartList from "./ShoppingCartList";
 
-
-
 export default async function CartPage() {
- const response = await fetch ('https://legendary-sniffle-5p99qxwj4rrh446j-3000.app.github.dev/api/users/1/cart')
- const cartProducts = await response.json();
+  const response = await fetch('https://legendary-sniffle-5p99qxwj4rrh446j-3000.app.github.dev/api/users/2/cart', {
+    cache: 'no-cache',
+  });
+  const cartProducts = await response.json();
 
-
- return <ShoppingCartList initialCartProducts={cartProducts} />
-
+  return (
+    <ShoppingCartList initialCartProducts={cartProducts} />
+  );
 }
